392b42f3ca10a61ffb3cd98c94257a55bc9e3cb7
Aleksandr <aleksandrbykanov1986@gmail.com>
2022-11-19 12:52:58 +0200

    Классы + наследование

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
-const slider = new SwipeCarousel();
+import SwipeCarousel from './carousel-swipe.js';
+
+const slider = new SwipeCarousel({
+  containerID: '#mycarousel', 
+  // slideID: '.item',
+  interval: 2000,
+  isPlaying: false,
+});
 
 slider.init();
+
+
+
+
+
+
